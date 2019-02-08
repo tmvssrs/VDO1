9db27ff31145468daa3bec7b852c624cee4cde39
TheBuyna <thebuyna@gmail.com>
2019-02-08 14:19:05 +0100

    changed port number.

--- _BACK_/server.js
+++ _BACK_/server.js
@@ -1,6 +1,17 @@
-var http = require('http');
+//var http = require('http');
+const X = require('express');
+const APP = X();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello Class!');
-}).listen(8080);
+const HTTP = require('http');
+const PORT = 1997;
+
+//HTTP.createServer(function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/html'});
+//  res.end('Hello World!');
+//}).listen(1999);
+
+APP.use('/',X.static(__dirname + '/_SITE_/'));
+
+APP.listen(PORT, () => {
+    console.log(`\r\nNODE ::: I started my back end server port ${PORT}.\r\n`);
+})
