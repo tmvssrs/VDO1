4a495bd9345e4c961bebe863b2ff34666442b2a6
Achillis <Bart.Van.der.Stappen@telenet.be>
2019-02-11 10:06:26 +0100

    start

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const X = require('express');
 const APP = X();
+const PORT = 1288;
+const http = require('http');
 
-const HTTP = require('http');
-const PORT = 1607;
 
-/*HTTP.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(PORT);*/
 
-APP.use('/project1', X.static(__dirname + '/project1/'));
-APP.use('/project2', X.static(__dirname + '/project2/'));
+//http.createServer(function (req, res) {
+//	console.log("server started");
+  //  res.writeHead(200, {'Content-Type': 'text/plain'});
+   // res.end('Hello Bart!');
+//}).listen(PORT);
+
+APP.use('/project1', X.static(__dirname + '/project 1/'));
+APP.use('/project2', X.static(__dirname + '/project 2/'));
 APP.listen(PORT, () => {
-   console.log(`\r\nNode ::: I started my back end server on port ${PORT}.\r\n`); 
+	console.log(`\r\nNODE ::: I started my back end server on port $(PORT).\r\n`);
 });
+
