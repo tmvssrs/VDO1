00a2d07a814cdf45a662768993376e513683aecc
Dries Mellaerts <dries.mellaerts@hotmail.be>
2019-02-11 13:54:36 +0100

    server.js

--- _BACK_/server.js
+++ _BACK_/server.js
@@ -8,6 +8,8 @@ const PORT = 1400;
 APP.use('/', X.static(__dirname + '/_SITE_/'));
 
 APP.get('/students', (req,res) => {res.send(["Jommeke","Filleberke"])});
+APP.get('/vakken', (req,res) => {res.send(["HTML","CSS"])});
+APP.get('/fouten', (req,res) => {res.send(["Hoofdletter vergeten","; vergeten"])});
 
 APP.listen(PORT, () => {
     console.log(`\r\nNODE ::: I started my back end server on port ${PORT}.\r\n`);
