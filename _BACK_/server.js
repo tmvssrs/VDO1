9d4ecccdb642b9e1ed22b04c762a8b60d4e7c115
Dries Mellaerts <dries.mellaerts@hotmail.be>
2019-02-11 11:27:40 +0100

    ini

--- _BACK_/server.js
+++ _BACK_/server.js
@@ -6,6 +6,9 @@ const APP = X();
 const PORT = 1400;
 
 APP.use('/', X.static(__dirname + '/_SITE_/'));
+
+APP.get('/students', (req,res) => {res.send(["Jommeke","Filleberke"])});
+
 APP.listen(PORT, () => {
     console.log(`\r\nNODE ::: I started my back end server on port ${PORT}.\r\n`);
 });
